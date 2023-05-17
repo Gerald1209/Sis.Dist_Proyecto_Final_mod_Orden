e05b4d1acbf5e5a3d22c39730f66ccc47f9348b8
MarlieR-321 <marlie.ramirez12169@est.uca.edu.ni>
2023-05-16 21:34:32 -0600

    Entidad Orden (relaciones lombok temporal)

--- businesdomain/orden/src/main/java/org/prolis/orden/entity/Orden.java
+++ businesdomain/orden/src/main/java/org/prolis/orden/entity/Orden.java
@@ -1,16 +1,55 @@
 package org.prolis.orden.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.prolis.empleado.entity.Empleado;
+import org.prolis.paciente.entity.Paciente;
+import org.prolis.tipo_orden.entity.TipoOrden;
+import org.prolis.tipo_servicio.entity.TipoServicio;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Date;
 
 @Entity
 @Data
 public class Orden {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idpais;
-    private String descripcion;
+    private Long IdOrden;
+
+    private String N_Orden;
+
+    private Long IdEmpleado;
+    private Long IdPaciente;
+    private Long IdTipoServicio;
+    private Long IdTipoOrden;
+
+    private String Asistencia;
+    private String Observaciones;
+    private Date FechaOrden;
+    private String Activo;
+    private LocalDateTime FechaImprime;
+    //creo que dijo que desde fecha imprime se pueden quitar los campos, si no, cambiar
+
+    //TBD : Investigar Funcionamiento de relaciones en LOMBOK --
+    //TODO: Para mientras queda asi https://medium.com/huawei-developers/database-relationships-in-spring-data-jpa-8d7181f50f60
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "IdEmpleado")
+    private Empleado empleado;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "IdPaciente")
+    private Paciente pacientes;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "IdTipoServicio")
+    private TipoServicio servicio;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "IdTipoOrden")
+    private TipoOrden orden;
+
+
 }
