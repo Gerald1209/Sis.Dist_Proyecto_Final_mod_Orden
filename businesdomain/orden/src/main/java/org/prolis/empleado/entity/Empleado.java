a8fa1166b0a2b1754c89bb3eb55687def25f61cc
MarlieR-321 <marlie.ramirez12169@est.uca.edu.ni>
2023-05-16 21:11:33 -0600

    entidad empleado y paciente

--- businesdomain/orden/src/main/java/org/prolis/empleado/entity/Empleado.java
+++ businesdomain/orden/src/main/java/org/prolis/empleado/entity/Empleado.java
@@ -1,16 +1,45 @@
 package org.prolis.empleado.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.Date;
 
 @Entity
 @Data
+@Table(name = "tbl_empleado")
 public class Empleado {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idpais;
-    private String descripcion;
+
+    private Long IdIdentificacion;
+    private Long IdPaisNac;
+    private Long IdDepartamentoNac;
+    private Long IdMunicipioNac;
+    private Long IdPaisRes;
+    private Long IdDepartamentoRes;
+    private Long IdMunicipioRes;
+    private Long IdEstadoCivil;
+    private Long IdSexo;
+    private Long IdNacionalidad;
+
+    private String NumIdentificacion;
+    private String NumINSS;
+    private String CodMinsa;
+
+    private String PrimerNombre;
+    private String SegundoNombre;
+    private String PrimerApellido;
+    private String SegundoApellido;
+    private Date FechaNac;
+    private String EdadIngreso;
+    private String Email;
+    private String DireccionDomiciliar;
+    private String TelefonoDomiciliar;
+    private String TelefonoMovil;
+    private Date FechaContrato;
+    private String UrlFoto;
+    private String Activo;
+    private int Estado;
 }
