008b758890f6c3f1d35fcee9943997c989d31dec
Gerald Diaz <gerald.diaz11715@est.uca.edu.ni>
2023-05-25 21:21:16 -0600

    Reinicio de todo

---
+++ businesdomain/mempleado/src/main/java/org/prolis/mempleado/MempleadoApplication.java
@@ -0,0 +1,13 @@
+package org.prolis.mempleado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MempleadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MempleadoApplication.class, args);
+    }
+
+}
---
+++ businesdomain/mempleado/src/test/java/org/prolis/mempleado/MempleadoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prolis.mempleado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MempleadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
